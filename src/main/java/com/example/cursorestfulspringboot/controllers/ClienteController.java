7a176b433556d85184c709f4370c329515a8a4cd
Daniel <daniel.dlimaf@gmail.com>
2020-09-26 23:24:42 -0300

    Aula 05

--- src/main/java/com/example/cursorestfulspringboot/controllers/ClienteController.java
+++ src/main/java/com/example/cursorestfulspringboot/controllers/ClienteController.java
@@ -5,9 +5,16 @@ import java.util.List;
 
 import javax.annotation.PostConstruct;
 
+import com.example.cursorestfulspringboot.model.Cliente;
+import com.example.cursorestfulspringboot.repository.ClienteRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
 /* 
 A classe clienteController é uma classe que o new dela é feita pelo spring
 E a partir dela constroi objetos, o responsável por criar os objetos é o spring quando usa 
@@ -16,69 +23,30 @@ Essa Classe possui os métodos init(), getCliente(), getClienteByCodigo()
 */
 @RestController
 public class ClienteController {
-
-  //colocando atributo na classe ClienteController
-  //Os objetos cliente serão construídos e serão armazenados nessa lista (atributo clientes do tipo List)
-  public List<Cliente>clientes; 
-  //List é uma classe, mas é preciso importar = ctrl + '.' e import 'List'(java.util)
-
-  //Um atributo faz parte da estrutura da classe e todo mundo enxerga ele
-  //Uma variável é visível apenas no método em que foi declarada
-  //Apenas classes, atributos e métodos podem ser públicas
-  //Esse método constrói um array com 2 clientes e é preciso guardar o array em algum lugar
   
-  //spring, assim que voce construir o objeto ClienteController chamar o método init()
-  //Significa após a contrução do objeto
-  @PostConstruct
-  public void init(){
-    Cliente c1 = new Cliente();
-    c1.id = 1;
-    c1.nome = "Jose";
-    c1.endereco = "Rua X, 99";
-    c1.saldo = 202.0;
+  //Anotação para o spring injetar o componente(objeto, ClienteRepository)aqui
+ //Injeção de dependencias
+ //Tirando o @component e o @autowired seria necessário fazer assim
+ //private ClienteRepository repository = new ClienteRepository();
+  @Autowired
+  private ClienteRepository repository;
 
-    Cliente c2 = new Cliente();
-    c2.id = 2;
-    c2.nome = "Maria";
-    c2.endereco = "Rua y, 59";
-    c2.saldo = 444.0;
-
-    Cliente c3 = new Cliente();
-    c3.id = 3;
-    c3.nome = "Fernanda";
-    c3.endereco = "Rua W, 33";
-    c3.saldo = 332.0;
-
-    //clientes é um atributo do tipo list que armazena um array de objetos
-    clientes = Arrays.asList(c1,c2,c3);
-    //Arrays é uma classe utilitaria "java.util"
-  }
-  
   //Esse método vai devolver a lista de clientes(objetos)
   @GetMapping("/clientes")
-  public List<Cliente> getCliente(){
-    return clientes;
+  public List<Cliente> getClientes(){
+    return repository.getAllClientes();
   }
 
   //Esse método vai devolver um cliente daquela lista
   @GetMapping("/clientes/{id}")
   public Cliente getClienteByCodigo(@PathVariable int id){
-    /*
-    c1 = null não é possivel fazer isso pois c1 é uma variável
-    declarada no método init
-    */
-    Cliente cliente = null;
-
-    //aux é um cara da lista
-    //clientes é o array lista, na primeira execução aux é o cliente de id 1, na segunda cliente de id 2
-    //na terceira cliente de id 3
-    for(Cliente aux : clientes){
-      if(aux.id == id){
-        cliente = aux; // cliente é uma variavel, clientes é o atributo, aux é uma variável do for
-        break;
-        //cliente e aux apontam para o mesmo objeto
-      }
-    }
-    return cliente;
+    return repository.getClienteById(id);
   }
+
+  @PostMapping("/clientes")
+  public Cliente salvar(@RequestBody Cliente cliente){
+    return repository.salvar(cliente);
+  }
+//Quando o postman mandar os dados, ele manda pelo body, o spring pegara esses dados
+//e irá mapear isso dentro do Cliente 
 }
---
+++ src/main/java/com/example/cursorestfulspringboot/repository/ClienteRepository.java
@@ -0,0 +1,77 @@
+package com.example.cursorestfulspringboot.repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import com.example.cursorestfulspringboot.model.Cliente;
+import org.springframework.stereotype.Component;
+//@component cria um componente do spring, eu posso usar esse componente
+//em qualquer lugar, e o spring constroi esse objeto
+//E quando constroi esse objeto automaticamente ele chama o init
+@Component
+public class ClienteRepository {
+  
+  //Os objetos Cliente serão construídos e serão armazenados nessa lista (atributo clientes do tipo List)
+  public List<Cliente>clientes; 
+  //List é uma classe, mas é preciso importar = ctrl + '.' e import 'List'(java.util)
+  //Pois para uma classe enxergar a outra é necessário importar (quando estão em pacotes(pastas) diferentes)
+
+  //Um atributo faz parte da estrutura da classe e todo mundo enxerga ele
+  //Uma variável é visível apenas no método em que foi declarada
+  //Apenas classes, atributos e métodos podem ser públicas
+  //Esse método constrói um array com 2 clientes e é preciso guardar o array em algum lugar
+  
+  //spring, assim que voce construir o objeto ClienteController chamar o método init()
+  //Significa após a contrução do objeto
+  @PostConstruct
+  public void init(){
+    Cliente c1 = new Cliente();
+    c1.setId(1);
+    c1.setNome("Jose");
+    c1.setEndereco("Rua X, 99");
+    c1.setSaldo(202.0);
+
+    Cliente c2 = new Cliente();
+    c2.setId(2);
+    c2.setNome("Maria");
+    c2.setEndereco( "Rua y, 59");
+    c2.setSaldo(444.0);
+
+    Cliente c3 = new Cliente();
+    c3.setId(3);
+    c3.setNome("Fernanda");
+    c3.setEndereco("Rua W, 33");
+    c3.setSaldo(332.0);
+
+    //clientes é um atributo do tipo list que armazena um array de objetos
+    clientes = new ArrayList<Cliente>();
+    clientes.add(c1);
+    clientes.add(c2);
+    clientes.add(c3);
+    //Arrays é uma classe utilitaria "java.util"
+    //clientes = Arrays.asList(c1,c2,c3) -> Cria uma lista de tam fixo
+    //Quando for salvar alguem nessa lista tem que usar esse formato
+  }
+  public List<Cliente> getAllClientes(){
+    return clientes;
+  }
+
+  public Cliente getClienteById(int id){
+    //aux é um cara da lista
+    //clientes é o array lista, na primeira execução aux é o cliente de id 1, na segunda cliente de id 2
+    //na terceira cliente de id 3
+    for(Cliente aux : clientes){
+      if(aux.getId() == id){
+        return aux; // cliente é uma variavel, clientes é o atributo, aux é uma variável do for
+        //cliente e aux apontam para o mesmo objeto
+      }
+    }
+    return null;
+  }
+  public Cliente salvar(Cliente cliente) {
+    cliente.setId(clientes.size() + 1); //será o cod do novo cliente
+    clientes.add(cliente);
+    return cliente;
+  }
+}
+//Tudo que manipula a lista ficou aqui
